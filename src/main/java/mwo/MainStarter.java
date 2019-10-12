210555d2ddd2b4dc4368ceedafa98793f19ed433
Kacper Zielinski <kacper.zielinski@capgemini.com>
2019-10-12 14:34:59 +0200

    Added SpringBootStarter class

---
+++ src/main/java/mwo/MainStarter.java
@@ -0,0 +1,12 @@
+package mwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainStarter.class, args);
+    }
+}

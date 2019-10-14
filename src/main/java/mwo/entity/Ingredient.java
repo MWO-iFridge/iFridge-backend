85f0c23f6486c62810654cca856817ef79d40ae2
Kacper Zieliński <kacper.zielinski@mailfence.com>
2019-10-14 17:48:10 +0200

    Added proper ID assign for Ingredient entity

--- src/main/java/mwo/entity/Ingredient.java
+++ src/main/java/mwo/entity/Ingredient.java
@@ -2,6 +2,8 @@ package mwo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -13,6 +15,7 @@ import lombok.Data;
 public class Ingredient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
